c03ac7a43c07a4591719e9d695abb61e98553438
arvind <achurox1121@gmail.com>
2025-01-19 19:57:10 +0530

    setup metamask and ganache

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useSDK } from "@metamask/sdk-react";
+import React from "react";
+import { useState } from "react";
+const App = () => {
+  const [account, setAccount] = useState();
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
 
-import './App.css'
-
-function App() {
-
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+    }
+  };
 
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className="App">
+      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+        Connect
+      </button>
+      {connected && (
+        <div>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {account && `Connected account: ${account}`}
+          </>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App
